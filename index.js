6af6e03d0d6a9dde3e5d07a032d05225f5f025a6
Mitwok <lysko72@gmail.com>
2023-11-26 20:00:26 +0100

    inputs added

--- index.js
+++ index.js
@@ -6,5 +6,41 @@ document.addEventListener("DOMContentLoaded", function () {
       const currencyData = data.data[0].rates;
       console.log(currencyData.map((currency) => currency.currency));
       console.log(currencyData.map((currency) => currency.code));
+      console.log(currencyData.map((currency) => currency));
     });
+
+  const giveInput = document.getElementById("give-input");
+  const receiveInput = document.getElementById("receive-input");
+  const swapButton = document.getElementById("swap-button");
+  const receiveCurrency = document.getElementById("receive-currency");
+  const giveCurrency = document.getElementById("give-currency");
+
+  let exchangeRate = 4.22;
+
+  receiveInput.placeholder = 100 * exchangeRate;
+
+  giveInput.addEventListener("keyup", function () {
+    if (giveInput.value >= 0.01) {
+      const receiveOutput = giveInput.value * exchangeRate;
+      receiveInput.value = receiveOutput.toFixed(2).replace(/\.?0*$/, "");
+    } else {
+      receiveInput.value = "";
+    }
+  });
+
+  receiveInput.addEventListener("keyup", function () {
+    if (receiveInput.value >= 0.01) {
+      const giveOutput = receiveInput.value / exchangeRate;
+      giveInput.value = giveOutput.toFixed(2).replace(/\.?0*$/, "");
+    } else {
+      giveInput.value = "";
+    }
+  });
+
+  swapButton.addEventListener("click", function () {
+    const newReceiveCurrency = giveCurrency.value;
+    const newGiveCurrency = receiveCurrency.value;
+    giveCurrency.value = newGiveCurrency;
+    receiveCurrency.value = newReceiveCurrency;
+  });
 });
